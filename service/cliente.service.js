733ec057aa0c450e6a239719d1a458308e809db1
Erick Rodrigues <profissional.erick@gmail.com>
2025-02-03 18:38:15 -0300

    funcao criar cliente

--- service/cliente.service.js
+++ service/cliente.service.js
@@ -8,21 +8,20 @@ const criaNovaLinha = (nome, email) => {
                         <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
                         <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                     </ul>
-                </td> -->`
+                </td> `
 
   linhaNovoCliente.innerHTML = conteudo;
   return linhaNovoCliente;
 }
 
 const tabela = document.querySelector('[data-tabela]');
+const formulario = document.querySelector('[data-form]');
 
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
   .then( res => {
     return res.json()
-
   })
-  
   }
 
   listaClientes()
@@ -32,14 +31,32 @@ const listaClientes = () => {
     })
   })
 
+  const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+        
+      
+    })
+    .then( res => {
+      return res.body
+    })
+  }
 
 
+formulario.addEventListener( 'submit', (evento) => {
+  evento.preventDefault()
+  const nome = evento.target.querySelector('[data-nome]').value;
+  const email = evento.target.querySelector('[data-email]').value;
 
- 
+  criaCliente(nome, email).then(() => {
+    window.location.href = '../telas/cadastro_concluido.html'
+  })
+})
 
-
-  /*http.onload = () => {
-      const data = JSON.parse(http.response)
-      data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    })}*/
